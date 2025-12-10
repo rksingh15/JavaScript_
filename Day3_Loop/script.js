2b4a2770674207c9713a05ecab87f734fb58eb4d
DrSkyRitik-15 <ritiksinghkushwaha06@gmail.com>
2025-12-10 12:12:57 +0530

    Add initial HTML structure and script file for JavaScript loops

---
+++ Day3_Loop/script.js
@@ -0,0 +1,64 @@
+//Loop
+
+
+
+/*
+for (let i = 1; i <= 10; i++) {
+    console.log("Iteration number: " + i);
+}
+*/
+
+/*
+let sum = 0;
+let n=prompt("Enter a number:");
+for (let j = 1; j <= n; j++) {
+    sum += j;
+}
+console.log("Sum of first " + n + " natural numbers is: " + sum);
+*/
+
+//for of loop
+
+/*
+let str = "Hello";
+for (let char of str) {
+    console.log(char); //H e l l o
+}
+
+let colors = ["Red", "Green", "Blue"];
+for (let color of colors) {
+    console.log(color);
+}
+//Red
+//Green
+//Blue
+
+//for in loop
+
+let arr = [10, 20, 30, 40, 50];
+for (let index in arr) {
+    console.log("Index: " + index + ", Value: " + arr[index]);
+}
+
+//length property
+
+let fruits = ["Apple", "Banana", "Mango", "Orange"];
+console.log("Number of fruits: " + fruits.length); //4
+
+for (let i = 0; i < fruits.length; i++) {
+    console.log(fruits[i]);
+}
+
+//Apple
+//Banana
+//Mango
+//Orange
+
+let strr="JavaScript";
+console.log("Length of string: " + strr.length); //10
+
+for (let i = 0; i < strr.length; i++) {
+    console.log(strr[i]);
+}
+
+*/
